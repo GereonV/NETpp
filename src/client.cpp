f967b6986746c734b3a00a6705fa05047ed79dcf
Gereon V <72784429+GereonV@users.noreply.github.com>
2022-08-23 18:02:24 +0200

    Improve connection io

--- src/client.cpp
+++ src/client.cpp
@@ -7,8 +7,7 @@ int main(int argc, char const ** argv) {
 			throw std::invalid_argument{"No IP address provided"};
 		net::context c;
 		auto ep = net::endpoints(argv[1], "13", net::STREAM);
-		char buf[256];
-		std::cout.write(buf, net::tcp::connection{ep->ai_addr, ep->ai_addrlen}.recv(buf));
+		std::cout << net::tcp::connection{ep->ai_addr, ep->ai_addrlen};
 	} catch(std::exception & e) {
 		std::cerr << e.what() << '\n';
 		return -1;
--- src/tcp.hpp
+++ src/tcp.hpp
@@ -1,6 +1,7 @@
 #ifndef _NET_TCP_HPP_
 #define _NET_TCP_HPP_
 
+#include <ostream>
 #include "socket.hpp"
 
 namespace net::tcp {
@@ -21,17 +22,32 @@ namespace net::tcp {
     public:
         connection(server const & server) : sock_{server.sock_.accept()} {}
         connection(void * addr, context::len_t addr_size) : sock_{addr, addr_size} { sock_.connect(); }
-        void send(char const * msg, auto len) { for(auto end = msg + len; len;) len -= sock_.send(end - len, len); }
-        template<std::size_t N> auto recv(char (&buf)[N]) { return sock_.recv(buf, N); }
+        void send(char const * msg, auto len) const { for(auto end = msg + len; len;) len -= sock_.send(end - len, len); }
+        template<std::size_t N> auto recv(char (&buf)[N]) const { return sock_.recv(buf, N); }
 
         template<std::size_t N>
-        auto operator<<(char const (&msg)[N]) {
+        auto operator<<(char const (&msg)[N]) const {
             send(msg, N);
             return *this;
         }
 
-        auto operator<<(auto msg) {
+        auto operator<<(auto msg) const {
             send(msg.data(), msg.size());
+            return *this;
+        }
+
+        friend auto & operator<<(std::ostream & os, connection const & conn) {
+            char buf[1024];
+            for(auto received = conn.recv(buf); received; received = conn.recv(buf))
+                os.write(buf, received);
+            return os;
+        }
+
+        auto operator>>(std::string & str) const {
+            str.clear();
+            char buf[1024];
+            for(auto received = recv(buf); received; received = recv(buf))
+                str.append(buf, received);
             return *this;
         }
 
@@ -41,4 +57,4 @@ namespace net::tcp {
 
 }
 
-#endif
+#endif // _NET_TCP_HPP_

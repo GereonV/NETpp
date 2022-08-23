3ae25ff8e00595bbdc29f87fa93fff64c4f71ce5
Gereon V <72784429+GereonV@users.noreply.github.com>
2022-08-23 22:29:36 +0200

    Reimplement connection-io

--- src/tcp.hpp
+++ src/tcp.hpp
@@ -22,35 +22,36 @@ namespace net::tcp {
     public:
         connection(server const & server) : sock_{server.sock_.accept()} {}
         connection(void * addr, context::len_t addr_size) : sock_{addr, addr_size} { sock_.connect(); }
-        void send(char const * msg, auto len) const { for(auto end = msg + len; len;) len -= sock_.send(end - len, len); }
-        template<std::size_t N> auto recv(char (&buf)[N]) const { return sock_.recv(buf, N); }
-
-        template<std::size_t N>
-        auto operator<<(char const (&msg)[N]) const {
-            send(msg, N);
-            return *this;
+        connection(addrinfo_list info) : connection{info.get()} {}
+        connection(addrinfo const * info) : sock_{info->ai_addr, info->ai_addrlen} {
+            auto family = info->ai_addr->sa_family;
+            for(info = info->ai_next; info; info = info->ai_next) {
+                try {
+                    sock_.connect();
+                    return;
+                } catch(std::runtime_error & e) {
+                    sock_.reset(info->ai_addr, info->ai_addrlen);
+                    auto new_fam = info->ai_addr->sa_family;
+                    if(new_fam == family)
+                        continue;
+                    sock_.close();
+                    sock_.resock();
+                    family = new_fam;
+                }
+            }
+            sock_.connect();
         }
 
-        auto operator<<(auto msg) const {
-            send(msg.data(), msg.size());
-            return *this;
+        auto & operator<<(auto && msg) const { return send(msg.data(), msg.size()), *this; }
+        auto & operator<<(char const * msg) const { return *this << std::string_view{msg}; }
+        template<std::size_t N> void send(char const (&buf)[N]) const { send(buf, N); }
+        void send(char const * buf, auto len) const {
+            for(auto end = buf + len; len;)
+                len -= sock_.send(end - len, len);
         }
 
-        friend auto & operator<<(std::ostream & os, connection const & conn) {
-            char buf[1024];
-            for(auto received = conn.recv(buf); received; received = conn.recv(buf))
-                os.write(buf, received);
-            return os;
-        }
-
-        auto operator>>(std::string & str) const {
-            str.clear();
-            char buf[1024];
-            for(auto received = recv(buf); received; received = recv(buf))
-                str.append(buf, received);
-            return *this;
-        }
-
+        template<std::size_t N> auto recv(char (&buf)[N]) const { return recv(buf, N); }
+        auto recv(char * buf, auto len) const { return sock_.recv(buf, len); }
     private:
         net::socket sock_;
     };
